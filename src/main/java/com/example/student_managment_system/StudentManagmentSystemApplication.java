0cbb411802c01e9d17f5e9504276889d45725f81
sardor <Sardorshavkatillayev@gmail.com>
2023-08-02 15:20:20 +0500

    commit test

---
+++ src/main/java/com/example/student_managment_system/StudentManagmentSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.student_managment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagmentSystemApplication.class, args);
+        System.out.println("Student-Managment-System");
+    }
+
+}
---
+++ src/test/java/com/example/student_managment_system/StudentManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_managment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
