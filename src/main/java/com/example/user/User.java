99c306dd7208da2a7bec26f87067d8ae5051b803
Elyor Azimov <elyorazimov098@gmail.com>
2023-08-03 10:13:13 +0500

    User entity created

---
+++ src/main/java/com/example/user/User.java
@@ -0,0 +1,29 @@
+package com.example.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+
+    private String surname;
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+}
